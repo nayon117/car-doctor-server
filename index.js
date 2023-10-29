bf240ac1d84c42a2a4b54136dc96fef4ac1c1f74
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-29 07:15:18 +0600

    get token and verify it

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const app = express();
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware 
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3hdabzk.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,6 +28,31 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares 
+const logger = async (req, res, next) => {
+  console.log('called', req.host, req.originalUrl);
+  next()
+} 
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('value of token in middleware', token);
+  if (!token) {
+    return res.status(401).send({message:'unauthorized'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // err 
+    if (err) {
+      console.log(err);
+      return res.status(401).send({message:'unauthorized'})
+    }
+    // if token is valid then decode it 
+    console.log('value in the token', decoded);
+    req.user = decoded;
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -30,7 +61,23 @@ async function run() {
       const serviceCollection = client.db('carDoctor').collection('services');
       const bookingCollection = client.db('carDoctor').collection('bookings');
 
-      app.get('/services', async (req, res) => {
+    // auth related api 
+    app.post('/jwt',logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure:false,
+        })
+        .send({success:true})
+    })
+    
+    
+    
+    // service related apis 
+      app.get('/services',logger, async (req, res) => {
           const cursor =  serviceCollection.find()
           const result = await cursor.toArray();
           res.send(result);
@@ -40,14 +87,30 @@ async function run() {
           const id = req.params.id;
           const query = { _id: new ObjectId(id) };
           const options = {
-            projection: { title: 1, price: 1, service_id: 1 },
+            projection: { title: 1, price: 1, service_id: 1, img:1 },
           };
       
           const result = await serviceCollection.findOne(query, options);
           res.send(result);
       })
 
-      //   bookings 
+    //   bookings
+    
+    app.get('/bookings',logger,verifyToken, async (req, res) => {
+      console.log(req.query.email);
+      console.log('user in the valid token', req.user);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({message:'forbidden access'})
+      }
+      let query = {}
+      if (req.query?.email) {
+        query ={email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray()
+      res.send(result)
+    })
+    
+
       app.post('/bookings', async (req, res) => { 
         const booking = req.body
         console.log(booking)
@@ -55,7 +118,27 @@ async function run() {
         res.send(result)
       })
 
-
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const updateBooking = req.body
+      console.log(updateBooking);
+      const updateDoc = {
+        $set: {
+          status:updateBooking.status
+        }
+      }
+      const result = await bookingCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result)
+      })
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
